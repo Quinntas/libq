94da255ba3617c0e148defa7bd4820977426744b
Caio "Quintas_" Santiago <13594102+Quinntas@users.noreply.github.com>
2024-07-14 14:35:53 -0300

    Initial commit

--- src/libq.c
+++ src/libq.c
@@ -5,34 +5,71 @@
 LinkedList* create_ll_node(void* data)
 {
     LinkedList* list = (LinkedList*)malloc(sizeof(LinkedList));
+    if (list == NULL)
+        return NULL;
     list->data = data;
     list->next = NULL;
     list->prev = NULL;
     return list;
 }
 
-void free_ll(LinkedList* list, void (*free_data)(void*))
+void free_ll(LinkedList* list, void (*free_data_cb)(void*))
 {
     LinkedList* current = list;
     while (current != NULL)
     {
         LinkedList* next = current->next;
-        if (free_data != NULL)
-            free_data(current->data);
+        if (free_data_cb != NULL)
+            free_data_cb(current->data);
         free(current);
         current = next;
     }
 }
 
-bool append_to_ll(LinkedList* list, void* data)
+int add_to_ll(LinkedList* list, void* data)
 {
-    LinkedList* current = list;
-    while (current->next != NULL)
-        current = current->next;
     LinkedList* new_node = create_ll_node(data);
-    current->next = new_node;
-    new_node->prev = current;
-    return true;
+    if (new_node == NULL)
+        return 0;
+    if (list->next == NULL)
+    {
+        list->next = new_node;
+        new_node->prev = list;
+        return 1;
+    }
+    else
+    {
+        LinkedList* next = list->next;
+        list->next = new_node;
+        new_node->prev = list;
+        new_node->next = next;
+        next->prev = new_node;
+        return 1;
+    }
+}
+
+int insert_to_ll(LinkedList* list, void* data, const int pos)
+{
+    LinkedList* new_node = create_ll_node(data);
+    if (new_node == NULL)
+        return 0;
+    LinkedList* current = list;
+    int i = 0;
+    while (current != NULL)
+    {
+        if (i == pos)
+        {
+            new_node->next = current->next;
+            new_node->prev = current;
+            current->next = new_node;
+            if (new_node->next != NULL)
+                new_node->next->prev = new_node;
+            return 1;
+        }
+        current = current->next;
+        i++;
+    }
+    return 0;
 }
 
 // Memory allocation
@@ -40,6 +77,8 @@ bool append_to_ll(LinkedList* list, void* data)
 char* alocate_string(const char* str)
 {
     char* new_str = (char*)malloc(strlen(str) + 1);
+    if (new_str == NULL)
+        return NULL;
     strcpy(new_str, str);
     return new_str;
 }
@@ -47,6 +86,8 @@ char* alocate_string(const char* str)
 int* alocate_int(const int value)
 {
     int* new_int = (int*)malloc(sizeof(int));
+    if (new_int == NULL)
+        return NULL;
     *new_int = value;
     return new_int;
 }
@@ -54,6 +95,8 @@ int* alocate_int(const int value)
 unsigned int* alocate_uint(const unsigned int value)
 {
     unsigned int* new_uint = (unsigned int*)malloc(sizeof(unsigned int));
+    if (new_uint == NULL)
+        return NULL;
     *new_uint = value;
     return new_uint;
 }
@@ -61,6 +104,8 @@ unsigned int* alocate_uint(const unsigned int value)
 float* alocate_float(const float value)
 {
     float* new_float = (float*)malloc(sizeof(float));
+    if (new_float == NULL)
+        return NULL;
     *new_float = value;
     return new_float;
 }
@@ -68,30 +113,15 @@ float* alocate_float(const float value)
 double* alocate_double(const double value)
 {
     double* new_double = (double*)malloc(sizeof(double));
+    if (new_double == NULL)
+        return NULL;
     *new_double = value;
     return new_double;
 }
 
-int get_data_int(LinkedList* list)
-{
-    return *((int*)list->data);
-}
-
-void free_data_int(void* data)
-{
-    printf("%d\n", *((int*)data));
-    free(data);
-}
-
 int main()
 {
-    LinkedList* list = create_ll_node(alocate_int(10));
-
-    printf("Data: %d\n", get_data_int(list));
-
-    free_ll(list, free_data_int);
-
-    printf("Data: %d\n", get_data_int(list));
+    printf("Hello from libq!\n");
 
     return 0;
 }
--- src/libq.h
+++ src/libq.h
@@ -2,5 +2,24 @@
 #define LIBQ_LIBRARY_H
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// Linked list
+
+typedef struct LinkedList
+{
+    void* data;
+    struct LinkedList* next;
+    struct LinkedList* prev;
+} LinkedList;
+
+LinkedList* create_ll_node(void* data);
+void free_ll(LinkedList* list, void (*free_data_cb)(void*));
+int add_to_ll(LinkedList* list, void* data);
+int insert_to_ll(LinkedList* list, void* data, const int pos);
+
+// Memory allocation
+char* alocate_string(const char* str);
 
 #endif //LIBQ_LIBRARY_H
